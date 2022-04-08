8fa86b131b8c661ca93bc94b7482f6decd57a1e7
A0321I1 - Hồ Quốc Kiên <hquockien@gmail.com>
2022-04-08 10:01:14 +0700

    update

--- src/main/java/mock/prj/finalp/repositories/UserInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package mock.prj.finalp.repositories;
-
-import mock.prj.finalp.model.UserInvoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInvoiceRepository extends JpaRepository<UserInvoice, Long> {
-}
--- src/main/java/mock/prj/finalp/services/impl/UserInvoiceServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package mock.prj.finalp.services.impl;
-
-import mock.prj.finalp.services.UserInvoiceService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserInvoiceServiceImpl implements UserInvoiceService {
-}
